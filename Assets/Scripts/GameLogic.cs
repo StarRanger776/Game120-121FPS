6bf3034e63ecc04e88cc21c0d95c29c4c9ff9237
Drelith <devinprather02@gmail.com>
2023-04-15 21:17:06 -0500

    Adding weapon 2
    ONLY STRUCTURE ADDED, NOT FULLY IMPLEMENTED AND PROB LOTS OF ISSUES!

--- Assets/Scripts/GameLogic.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameLogic : MonoBehaviour
-{
-    public bool enablePlayerMovementControls, enablePlayerCameraControls;
-    public float timeScale = 1.0f;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Game120-121FPS
-This is the FPS project for Game 120, and Game 121.
