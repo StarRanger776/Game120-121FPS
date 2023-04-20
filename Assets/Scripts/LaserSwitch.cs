c175df92757a0fc471ae9ce950212b83bd510a72
StarRanger776 <chrisfe21937@gmail.com>
2023-04-20 00:30:51 -0500

    Finished Switch and LaserGun Movement
    The Switch activates a Gun that is attached to it and makes it spin and grow

--- Assets/Scripts/LaserSwitch.cs
+++ Assets/Scripts/LaserSwitch.cs
@@ -1,14 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LaserSwitch : MonoBehaviour
 {
     public MeshRenderer meshRenderer;
     public Color color1;
     public Color color2;
-    public void ActivateLaser()
+    public TextMeshPro activateText;
+    public bool activated = false;
+    public bool withinBounds = false;
+    public LaserGun laserGun;
+
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E) && activated == false && withinBounds == true)
+        {
+            ActivateSwitch();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            activateText.gameObject.SetActive(true);
+            withinBounds = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            activateText.gameObject.SetActive(false);
+            withinBounds = false;
+        }
+    }
+
+    public void ActivateSwitch()
+    {
+        laserGun.called = true;
+        activated = true;
+        activateText.text = "Activated";
         meshRenderer.material.color = color2;
     }
 }
