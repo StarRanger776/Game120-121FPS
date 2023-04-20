0443fef87338ba1f67d6f9bb577665c794ec78f8
ZDuncan3 <zduncan3@stumail.jccc.edu>
2023-04-20 15:15:15 -0500

    Remove unused skyboxes to save space
    We will use the Pink Galaxy skybox

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    public PlayerController player;
+    private PlayerController player;
+    private Camera _mainCamera;
+    private GameObject _fpCam;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerController>();
+        _mainCamera = Camera.main;
+        _fpCam = FindObjectOfType<FirstPerson>().gameObject;
+
+        player.gameLogic.enablePlayerCameraControls = false;
+        player.gameLogic.enablePlayerGravity = false;
+        player.gameLogic.enablePlayerMovementControls = false;
+
+        player.gameObject.SetActive(false);
+        _mainCamera.gameObject.SetActive(false);
+        _fpCam.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
 }
--- Assets/Scripts/ShopButtons.cs
+++ Assets/Scripts/ShopButtons.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShopButtons : MonoBehaviour
 {
     private PlayerController player;
+    public string sceneToLoad;
+    private Camera _mainCamera;
+    public Vector3 spawnPosition;
+    private GameObject _fpCam;
 
-    private void Start()
+    private void Awake()
     {
         player = FindObjectOfType<PlayerController>();
+        _mainCamera = Camera.main;
+        _fpCam = FindObjectOfType<FirstPerson>().gameObject;
+    }
+
+    public void NextLevel()
+    {
+        player.gameLogic.enablePlayerMovementControls = true;
+        player.gameLogic.enablePlayerCameraControls = true;
+        player.gameLogic.enablePlayerGravity = true;
+        player.transform.position = spawnPosition;
+
+        _fpCam.SetActive(true);
+        _mainCamera.gameObject.SetActive(true);
+        player.gameObject.SetActive(true);
+
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
