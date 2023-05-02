bf58eafb12781fe34e0b189100f0597b7f12f8eb
RichardAVernon <128616263+RichardAVernon@users.noreply.github.com>
2023-05-02 03:59:36 -0500

    Main Menu UI
    Added in the Sensitivity Slider, if the Programmers could set that up to actually affect the PlayerController for me I'd be over the moon.
    
    Besides that I also added the Options menu, which basically just disabled and enables whatever menu is open ie; the "Start" menu and the "Options" menu.
    
    Also removed a script that I made a bit ago called Settings, which I had thought of using to implement a Save/Load feature that was demonstrated to me and a few of my peers in Cameron's Class, however, I obviously wouldn't have enough time to implement that.

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -4,22 +4,26 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MenuButtons : MonoBehaviour
 {
     public List<GameObject> listToEnable = new List<GameObject>();
     public bool overrideDefaultSceneLoad = false; // only for dev environment, gameLogic settings do not get changed by 
     public string sceneToLoad;
+    
     public Slider mouseSensSlider;
+    public TMP_Text sliderText;
+   
     private PlayerController player;
 
-    //Assume these change the formatting in the Editor, however Unity will not allow me to run the game without these being resolved.
+    public GameObject mainCanvas;
+    public GameObject optionsCanvas;
 
     private void Awake()
     {
         player = FindObjectOfType<PlayerController>();
 
-//<<<<<<< HEAD
         if (mouseSensSlider != null)
         {
             mouseSensSlider.maxValue = 100;
@@ -30,8 +34,13 @@ public class MenuButtons : MonoBehaviour
         }
     }
 
-//=======
-//>>>>>>> parent of 09c5c22 (Shop and Music)
+    private void Start()
+    {
+        //Just making sure that options is not active.
+        mainCanvas.SetActive(true);
+        optionsCanvas.SetActive(false);
+    }
+
     public void StartGame()
     {
         if (!overrideDefaultSceneLoad)
@@ -57,4 +66,30 @@ public class MenuButtons : MonoBehaviour
         // EditorApplication.isPlaying = false; // comment out or delete before building, else the project will fail to build
         Application.Quit();
     }
+
+    //Options
+
+    //Dunno if this is quite the most efficient way to do so, however I think a little bit can be given on the title screen.
+    public void OptionsButton()
+    {
+        mainCanvas.SetActive(false);
+        optionsCanvas.SetActive(true);
+    }
+
+    private void Update()
+    {
+        //There might be a better place to put it, but for now I am stashing it in here, because that makes the most sense to me.
+        if (sliderText != null)
+        {
+            float formattedValue = mouseSensSlider.value / 10f;
+            sliderText.text = "Sensitivity:" + formattedValue.ToString("0.0");
+        }
+    }
+
+    public void OptionsBack() 
+    {
+        mainCanvas.SetActive(true);
+        optionsCanvas.SetActive(false);
+    }
+
 }
